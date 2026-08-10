4f7515dbd4d55f17af035546e2ef33c17c3a0faf
Myshko.Mykhailo <malko.mykhailo@outlook.com>
2026-08-10 23:02:24 +0300

    add 4th task

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
-
+const form = document.querySelector(".login-form");
+const filledForm = {};
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (
+    !event.currentTarget.elements.email.value.trim() ||
+    !event.currentTarget.elements.password.value.trim()
+  ) {
+    alert("All form fields must be filled in");
+    return;
+  } else {
+    filledForm.password = event.currentTarget.elements.password.value.trim();
+    filledForm.email = event.currentTarget.elements.email.value.trim();
+  }
+  console.log(filledForm);
+  event.currentTarget.reset();
+});
