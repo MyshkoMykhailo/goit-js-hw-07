d57d19c35c76856f267be4acd26235b258686cd5
Myshko.Mykhailo <malko.mykhailo@outlook.com>
2026-08-10 23:17:22 +0300

    add 5th task

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector("body");
+const changeColorBtn = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+changeColorBtn.addEventListener("click", (event) => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  spanColor.textContent = `${color}`;
+});
